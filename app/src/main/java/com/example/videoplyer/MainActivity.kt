835dbc55a10cc9030040dfd3c207ce5a004ef05c
ishwa <ishwarbhaidesai202gnu.ac.in>
2022-10-12 21:23:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/videoplyer/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.videoplyer
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.MediaController
+import android.widget.Toast
+import android.widget.VideoView
+
+class MainActivity : AppCompatActivity() {
+
+    var videoView: VideoView? = null
+
+    var mediaController : MediaController? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        videoView = findViewById<View>(R.id.videoView) as VideoView?
+
+        if (mediaController == null) {
+            mediaController = MediaController(this)
+            mediaController!!.setAnchorView(this.videoView)
+        }
+
+        videoView!!.setMediaController(mediaController)
+
+        videoView!!.setVideoURI(Uri.parse("android.resource://"+packageName+"/"+R.raw.video))
+
+        videoView!!.requestFocus()
+
+        videoView!!.start()
+
+        videoView!!.setOnCompletionListener {
+
+            Toast.makeText(applicationContext, "Video End", Toast.LENGTH_SHORT).show()
+
+        }
+
+        videoView!!.setOnErrorListener { mediaPlayer, i, i2 ->
+
+            Toast.makeText(applicationContext, "Error Occured", Toast.LENGTH_SHORT).show()
+            false
+        }
+
+        }
+
+    }
